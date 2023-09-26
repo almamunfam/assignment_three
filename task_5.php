9587ff25b9633592ba0a35abfb124def735ea379
almamunfm <learneralmamun@gmail.com>
2023-09-26 13:20:47 +0600

    assignment three done

---
+++ task_5.php
@@ -0,0 +1,19 @@
+<?php
+function generatePassword($length) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = mt_rand(0, strlen($chars) - 1);
+        $password .= $chars[$randomIndex];
+    }
+
+    return $password;
+}
+
+// Generate a password with a length of 12 characters
+$password = generatePassword(12);
+
+// Print the generated password
+echo "Generated Password: " . $password . "\n";
+?>
